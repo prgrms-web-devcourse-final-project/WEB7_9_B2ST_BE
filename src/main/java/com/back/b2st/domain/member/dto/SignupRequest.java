77d43a7d28d8d461c570d915fb8f5c5c066f13d8
weilim0513-tech <weilim0513@gmail.com>
2025-12-09 20:54:36 +0900

    feat: member entity, requet dto 추가

---
+++ src/main/java/com/back/b2st/domain/member/dto/SignupRequest.java
@@ -0,0 +1,34 @@
+package com.back.b2st.domain.member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class SignupRequest {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    // 비밀번호는 8~30자, 영문자+숫자+특수기호 포함
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,30}$",
+            message = "비밀번호는 8~30자, 영소문자+숫자+특수기호를 포함해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "이름은 필수입니다.")
+    private String name;
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+
+    private String phone;
+
+    private LocalDate birth;
+}

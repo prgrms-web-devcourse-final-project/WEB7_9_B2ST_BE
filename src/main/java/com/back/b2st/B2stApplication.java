e0e710864d2fc4c6563b0ce6a1f39a7f56345a6c
minibr <minimin9823@gmail.com>
2025-12-09 09:10:48 +0900

    Initial commit

---
+++ src/main/java/com/back/b2st/B2stApplication.java
@@ -0,0 +1,13 @@
+package com.back.b2st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2stApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/b2st/B2stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.b2st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

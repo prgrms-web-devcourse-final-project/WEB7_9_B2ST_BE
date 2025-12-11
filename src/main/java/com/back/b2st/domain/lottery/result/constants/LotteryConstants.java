44c5246bfc03982f60d1210f35e1cc32a58be8e9
노미경 <codingworld@naver.com>
2025-12-11 22:16:04 +0900

    feat: 상수 클래스 추가, seatId 삭제, index 중복, 오탈자 수정

---
+++ src/main/java/com/back/b2st/domain/lottery/result/constants/LotteryConstants.java
@@ -0,0 +1,10 @@
+package com.back.b2st.domain.lottery.result.constants;
+
+public class LotteryConstants {
+
+	private LotteryConstants() {    // 인스턴스 생성 방지
+	}
+
+	public static final int PAYMENT_DEADLINE_DAYS = 3;
+
+}
--- src/main/java/com/back/b2st/domain/lottery/result/entity/LotteryResult.java
+++ src/main/java/com/back/b2st/domain/lottery/result/entity/LotteryResult.java
@@ -2,6 +2,7 @@ package com.back.b2st.domain.lottery.result.entity;
 
 import java.time.LocalDateTime;
 
+import com.back.b2st.domain.lottery.result.constants.LotteryConstants;
 import com.back.b2st.global.jpa.entity.BaseEntity;
 
 import jakarta.persistence.Column;
@@ -23,10 +24,9 @@ import lombok.NoArgsConstructor;
 @Table(
 	name = "lottery_results",
 	indexes = {
+		@Index(name = "idx_lottery_results_lottery_entry", columnList = "lottery_entry_id"),
 		@Index(name = "idx_lottery_results_member", columnList = "member_id"),
-		@Index(name = "idx_lottery_results_schedule", columnList = "schedule_id"),
-		@Index(name = "idx_lottery_results_member_schedule", columnList = "member_id, schedule_id"),
-		@Index(name = "idx_lottery_results_payment_deadline", columnList = "payment_deadline"),
+		@Index(name = "idx_lottery_results_lottery_entry_member", columnList = "lottery_entry_id, member_id"),
 		@Index(name = "idx_lottery_results_payment_deadline", columnList = "payment_deadline")
 	}
 )
@@ -48,26 +48,21 @@ public class LotteryResult extends BaseEntity {
 	@Column(name = "member_id", nullable = false)
 	private Long memberId;
 
-	@Column(name = "seat_id", nullable = false)
-	private Long seatId;
-
 	@Column(name = "payment_deadline", nullable = false)
 	private LocalDateTime paymentDeadline;
 
 	@Column(name = "is_paid", nullable = false)
-	private boolean paid = false;
+	private boolean paid;
 
 	@Builder
 	public LotteryResult(
 		Long lotteryEntryId,
-		Long memberId,
-		Long seatId,
-		LocalDateTime paymentDeadline
+		Long memberId
 	) {
 		this.lotteryEntryId = lotteryEntryId;
 		this.memberId = memberId;
-		this.seatId = seatId;
-		this.paymentDeadline = LocalDateTime.now().plusDays(3);    // 생성일 + 3일
+		this.paymentDeadline = LocalDateTime.now()
+			.plusDays(LotteryConstants.PAYMENT_DEADLINE_DAYS);    // TODO : 생성일 + 3일(임시)
 		this.paid = false;
 	}
 }

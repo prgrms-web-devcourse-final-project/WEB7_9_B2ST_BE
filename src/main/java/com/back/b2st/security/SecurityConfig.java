a07dc964b89c952e2215f11e47bedef925c213a4
weilim0513-tech <weilim0513@gmail.com>
2025-12-11 14:32:24 +0900

    fix: h2 콘솔 500 미해결로, 일단 임시로 수정했던 csrf 다시 원복

--- src/main/java/com/back/b2st/security/SecurityConfig.java
+++ src/main/java/com/back/b2st/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,9 +31,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf
-				.ignoringRequestMatchers("/h2-console/**") // H2 콘솔은 CSRF 예외 처리
-			)
+			.csrf(AbstractHttpConfigurer::disable)
 			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
 
 			.authorizeHttpRequests(auth -> auth

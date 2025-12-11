d0d75a0a751cce028dee57720aef3d8580b719a3
Nomi <32328758+77r77r@users.noreply.github.com>
2025-12-11 14:07:54 +0900

    feat: 티켓 상태 변경 메소드 구현 (#28)
    * feat: Ticket 상태 변경 구현
    
    * feat: Ticket 상태 만료변경 구현
    
    * feat: Ticket 상태변경 서비스코드 구현
    
    * refactor: 메소드명 변경(checkTicket -> getTicket)

---
+++ src/main/java/com/back/b2st/domain/ticket/error/TicketErrorCode.java
@@ -0,0 +1,24 @@
+package com.back.b2st.domain.ticket.error;
+
+import org.springframework.http.HttpStatus;
+
+import com.back.b2st.global.error.code.ErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TicketErrorCode implements ErrorCode {
+
+	ALREADY_REGISTERED_TICKET(HttpStatus.BAD_REQUEST, "T001", "이미 등록된 티켓입니다."),
+	TICKET_NOT_FOUND(HttpStatus.NOT_FOUND, "T002", "티켓을 찾을 수 없습니다."),
+	ALREADY_CANCEL_TICKET(HttpStatus.BAD_REQUEST, "T003", "이미 취소된 티켓입니다."),
+	TICKET_NOT_CANCELABLE(HttpStatus.BAD_REQUEST, "T004", "취소할 수 없는 티켓입니다."),
+	CREATE_TICKET_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "T005", "티켓 등록 중 오류가 발생했습니다."),
+	CANCEL_TICKET_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "T006", "티켓 취소중 오류가 발생했습니다.");
+
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
+}
--- src/test/java/com/back/b2st/domain/ticket/service/TicketServiceTest.java
+++ src/test/java/com/back/b2st/domain/ticket/service/TicketServiceTest.java
@@ -2,20 +2,37 @@ package com.back.b2st.domain.ticket.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.back.b2st.domain.ticket.entity.Ticket;
+import com.back.b2st.domain.ticket.entity.TicketStatus;
 import com.back.b2st.domain.ticket.repository.TicketRepository;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class TicketServiceTest {
 
 	@Autowired
 	private TicketRepository ticketRepository;
+	@Autowired
+	private TicketService ticketService;
+
+	private Ticket ticket;
+
+	@BeforeEach
+	void setUp() {
+		Long rId = 2L;
+		Long mId = 2L;
+		Long sId = 4L;
+
+		ticket = ticketService.createTicket(rId, mId, sId);
+	}
 
 	@Test
 	void 티켓생성() {
@@ -35,6 +52,101 @@ class TicketServiceTest {
 		// DB 검증
 		Ticket findTicket = ticketRepository.findById(saveTicket.getId()).orElseThrow();
 		assertThat(findTicket).isEqualTo(saveTicket);
+	}
+
+	@Test
+	void 티켓생성_중복요청() {
+		// TODO 개발 필요
+	}
+
+	@Test
+	void 티켓_취소변경() {
+		// when
+		Long rId = ticket.getReservationId();
+		Long mId = ticket.getMemberId();
+		Long sId = ticket.getSeatId();
+
+		// when
+		ticketService.cancelTicket(rId, mId, sId);
+
+		// then
+		assertThat(ticket.getStatus()).isEqualTo(TicketStatus.CANCELED);
+
+		// DB 검증
+		Ticket findTicket = ticketRepository.findById(ticket.getId()).orElseThrow();
+		assertThat(findTicket.getStatus()).isEqualTo(TicketStatus.CANCELED);
+	}
+
+	@Test
+	void 티켓_사용변경() {
+		// when
+		Long rId = ticket.getReservationId();
+		Long mId = ticket.getMemberId();
+		Long sId = ticket.getSeatId();
+
+		// when
+		ticketService.useTicket(rId, mId, sId);
+
+		// then
+		assertThat(ticket.getStatus()).isEqualTo(TicketStatus.USED);
+
+		// DB 검증
+		Ticket findTicket = ticketRepository.findById(ticket.getId()).orElseThrow();
+		assertThat(findTicket.getStatus()).isEqualTo(TicketStatus.USED);
+	}
+
+	@Test
+	void 티켓_교환변경() {
+		// when
+		Long rId = ticket.getReservationId();
+		Long mId = ticket.getMemberId();
+		Long sId = ticket.getSeatId();
+
+		// when
+		ticketService.exchangeTicket(rId, mId, sId);
+
+		// then
+		assertThat(ticket.getStatus()).isEqualTo(TicketStatus.EXCHANGED);
+
+		// DB 검증
+		Ticket findTicket = ticketRepository.findById(ticket.getId()).orElseThrow();
+		assertThat(findTicket.getStatus()).isEqualTo(TicketStatus.EXCHANGED);
+	}
+
+	@Test
+	void 티켓_양도변경() {
+		// when
+		Long rId = ticket.getReservationId();
+		Long mId = ticket.getMemberId();
+		Long sId = ticket.getSeatId();
+
+		// when
+		ticketService.transferTicket(rId, mId, sId);
+
+		// then
+		assertThat(ticket.getStatus()).isEqualTo(TicketStatus.TRANSFERRED);
+
+		// DB 검증
+		Ticket findTicket = ticketRepository.findById(ticket.getId()).orElseThrow();
+		assertThat(findTicket.getStatus()).isEqualTo(TicketStatus.TRANSFERRED);
+	}
+
+	@Test
+	void 티켓_만료변경() {
+		// when
+		Long rId = ticket.getReservationId();
+		Long mId = ticket.getMemberId();
+		Long sId = ticket.getSeatId();
+
+		// when
+		ticketService.expireTicket(rId, mId, sId);
+
+		// then
+		assertThat(ticket.getStatus()).isEqualTo(TicketStatus.EXPIRED);
+
+		// DB 검증
+		Ticket findTicket = ticketRepository.findById(ticket.getId()).orElseThrow();
+		assertThat(findTicket.getStatus()).isEqualTo(TicketStatus.EXPIRED);
 	}
 
 }

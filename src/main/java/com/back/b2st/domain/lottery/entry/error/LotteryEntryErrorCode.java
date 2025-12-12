21605444c23d524b6000333945fc2b7d02efa5e1
노미경 <codingworld@naver.com>
2025-12-13 01:24:50 +0900

    refactor: ErrorCode 번호 수정

--- src/main/java/com/back/b2st/domain/lottery/entry/error/LotteryEntryErrorCode.java
+++ src/main/java/com/back/b2st/domain/lottery/entry/error/LotteryEntryErrorCode.java
@@ -16,8 +16,8 @@ public enum LotteryEntryErrorCode implements ErrorCode {
 	INVALID_SCHEDULE_INFO(HttpStatus.BAD_REQUEST, "L403", "회차 정보가 올바르지 않습니다."),
 	PERFORMANCE_SCHEDULE_MISMATCH(HttpStatus.BAD_REQUEST, "L404", "공연 정보와 회차 정보가 일치하지 않습니다."),
 	INVALID_GRADE_INFO(HttpStatus.BAD_REQUEST, "L405", "등급 정보가 올바르지 않습니다."),
-	EXCEEDS_MAX_ALLOCATION(HttpStatus.BAD_REQUEST, "L409", "응모 가능 수량은 초과할 수 없습니다."),
-	DUPLICATE_ENTRY(HttpStatus.CONFLICT, "L409", "이미 존재하는 응모 내역이 있습니다."),
+	EXCEEDS_MAX_ALLOCATION(HttpStatus.BAD_REQUEST, "L406", "응모 가능 수량은 초과할 수 없습니다."),
+	DUPLICATE_ENTRY(HttpStatus.CONFLICT, "L407", "이미 존재하는 응모 내역이 있습니다."),
 
 	CREATE_ENTRY_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "L501", "추첨응모 등록 중 서버 오류가 발생했습니다.");
 

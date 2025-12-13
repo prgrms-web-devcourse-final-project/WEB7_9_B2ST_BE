7643b045cebe2ab50bacc24f68664266652801cf
yeongin <duddlsdlek610@gmail.com>
2025-12-13 17:27:38 +0900

    feat: 예매 서비스 로직 추가

--- src/main/java/com/back/b2st/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/back/b2st/domain/reservation/controller/ReservationController.java
@@ -1,7 +1,47 @@
 package com.back.b2st.domain.reservation.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.back.b2st.domain.reservation.dto.request.ReservationRequest;
+import com.back.b2st.domain.reservation.dto.response.ReservationResponse;
+import com.back.b2st.domain.reservation.service.ReservationService;
+import com.back.b2st.global.common.BaseResponse;
+import com.back.b2st.security.CustomUserDetails;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/reservations")
 public class ReservationController {
+
+	private final ReservationService reservationService;
+
+	/** === 예매 생성 === */
+	@PostMapping
+	public BaseResponse<ReservationResponse> createReservation(
+		@AuthenticationPrincipal CustomUserDetails user,
+		@RequestBody ReservationRequest request
+	) {
+		// TODO: 추후 서비스단으로 로직 이동..?
+		Long memberId = user.getId();
+
+		ReservationResponse response = reservationService.createReservation(memberId, request);
+		return BaseResponse.success(response);
+	}
+
+	/** === 예매 단건 조회 === */
+	@GetMapping("/{reservationId}")
+	public BaseResponse<ReservationResponse> getReservation(
+		@PathVariable Long reservationId
+	) {
+		// TODO: 추후 구현 예정
+		return BaseResponse.success();
+	}
 }
--- src/main/java/com/back/b2st/domain/reservation/service/ReservationService.java
+++ src/main/java/com/back/b2st/domain/reservation/service/ReservationService.java
@@ -1,7 +1,45 @@
 package com.back.b2st.domain.reservation.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.back.b2st.domain.reservation.dto.request.ReservationRequest;
+import com.back.b2st.domain.reservation.dto.response.ReservationResponse;
+import com.back.b2st.domain.reservation.entity.Reservation;
+import com.back.b2st.domain.reservation.repository.ReservationRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class ReservationService {
+
+	private final SeatSelectionService seatSelectionService;
+	private final ReservationRepository reservationRepository;
+
+	/** === 예매 생성 === */
+	@Transactional
+	public ReservationResponse createReservation(Long memberId, ReservationRequest request) {
+
+		// 1. 좌석 HOLD 진행
+		seatSelectionService.holdSeat(request.performanceId(), request.seatId());
+
+		// 2. Reservation 엔티티 생성
+		Reservation reservation = Reservation.builder()
+			.performanceId(request.performanceId())
+			.memberId(memberId)
+			.seatId(request.seatId())
+			.build();
+
+		Reservation savedReservation = reservationRepository.save(reservation);
+
+		// 3. Response 변환
+		return new ReservationResponse(
+			savedReservation.getId(),
+			savedReservation.getMemberId(),
+			savedReservation.getPerformanceId(),
+			savedReservation.getSeatId()
+		);
+	}
+
 }

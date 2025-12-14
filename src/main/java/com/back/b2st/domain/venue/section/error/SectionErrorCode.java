75f50bd1b1c9da015de9c45a60fa7b8c5599ddb7
노미경 <codingworld@naver.com>
2025-12-14 23:09:38 +0900

    feat: 구역ID로 Section 조회

--- src/main/java/com/back/b2st/domain/venue/section/error/SectionErrorCode.java
+++ src/main/java/com/back/b2st/domain/venue/section/error/SectionErrorCode.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 public enum SectionErrorCode implements ErrorCode {
 	INVALID_VENUE_INFO(HttpStatus.BAD_REQUEST, "E401", "공연장 정보가 올바르지 않습니다."),
 	DUPLICATE_SECTION(HttpStatus.CONFLICT, "E402", "이미 등록된 구역입니다."),
+	SECTION_NOT_FOUND(HttpStatus.NOT_FOUND, "E002", "구역 정보를 찾을 수 없습니다."),
 
 	CREATE_SECTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "E501", "구역 등록 중 서버 오류가 발생했습니다.");
 

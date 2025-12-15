6363f6cd556b3c2720cb17fd4e0d33dcc4384567
yeongin <duddlsdlek610@gmail.com>
2025-12-16 01:50:52 +0900

    feat: 디티오, 상태 추가 및 수정

--- src/main/java/com/back/b2st/domain/reservation/entity/ReservationStatus.java
+++ src/main/java/com/back/b2st/domain/reservation/entity/ReservationStatus.java
@@ -3,6 +3,7 @@ package com.back.b2st.domain.reservation.entity;
 public enum ReservationStatus {
 	PENDING,    // 결제 대기 (무통장)
 	PAID,    // 결제 완료
-	CANCELLED,    // 유효하지 않은 예매
+	COMPLETED, // 확정
+	CANCELED,    // 취소
 	EXPIRED    // 결제 기한 초과
 }

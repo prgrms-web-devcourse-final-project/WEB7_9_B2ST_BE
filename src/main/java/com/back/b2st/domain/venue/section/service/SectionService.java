76b8c7196e6e66a3c544d3cce8c50ab9b6afde1b
Nomi <32328758+77r77r@users.noreply.github.com>
2025-12-16 08:54:53 +0900

    feat: 공연장 id 로 구역 조회 구현 (#123)
    * refactor: CreateSectionRes 정적 팩토리 변환
    
    * feat: 공연장 id로 조회
    
    * test: initData 생성으로 인한 testcase 수정

--- src/main/java/com/back/b2st/domain/venue/section/service/SectionService.java
+++ src/main/java/com/back/b2st/domain/venue/section/service/SectionService.java
@@ -1,12 +1,16 @@
 package com.back.b2st.domain.venue.section.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.back.b2st.domain.venue.section.dto.request.CreateSectionReq;
 import com.back.b2st.domain.venue.section.dto.response.CreateSectionRes;
+import com.back.b2st.domain.venue.section.dto.response.SectionInfoRes;
 import com.back.b2st.domain.venue.section.entity.Section;
 import com.back.b2st.domain.venue.section.error.SectionErrorCode;
 import com.back.b2st.domain.venue.section.repository.SectionRepository;
+import com.back.b2st.domain.venue.venue.repository.VenueRepository;
 import com.back.b2st.global.error.exception.BusinessException;
 
 import jakarta.validation.Valid;
@@ -17,7 +21,9 @@ import lombok.RequiredArgsConstructor;
 public class SectionService {
 
 	private final SectionRepository sectionRepository;
+	private final VenueRepository venueRepository;
 
+	// 구역 정보 생성
 	public CreateSectionRes createSectionInfo(Long venueId, @Valid CreateSectionReq request) {
 		validateVenueId(venueId);
 		validateSectionNotDuplicated(venueId, request.sectionName());
@@ -26,21 +32,34 @@ public class SectionService {
 			.sectionName(request.sectionName())
 			.build();
 
-		return new CreateSectionRes(sectionRepository.save(section));
+		return CreateSectionRes.form(sectionRepository.save(section));
 	}
 
+	// 기등록 데이터 검증
 	private void validateSectionNotDuplicated(Long venueId, String sectionName) {
 		if (sectionRepository.existsByVenueIdAndSectionName(venueId, sectionName)) {
 			throw new BusinessException(SectionErrorCode.DUPLICATE_SECTION);
 		}
 	}
 
+	// 공연장 검증
 	private void validateVenueId(Long venueId) {
-		// todo: check venueId
+		if (!venueRepository.existsById(venueId)) {
+			throw new BusinessException(SectionErrorCode.INVALID_VENUE_INFO);
+		}
 	}
 
+	// 구역 정보 조회 - 구역 Id
 	public Section getSection(Long sectionId) {
 		return sectionRepository.findById(sectionId)
 			.orElseThrow(() -> new BusinessException(SectionErrorCode.SECTION_NOT_FOUND));
+	}
+
+	// 구역 정보 조회 - 공연장 Id
+	public SectionInfoRes getSectionByVenueId(Long venueId) {
+		validateVenueId(venueId);
+		List<Section> sections = sectionRepository.findByVenueId(venueId);
+
+		return SectionInfoRes.from(venueId, sections);
 	}
 }
--- src/test/java/com/back/b2st/domain/venue/section/service/SectionServiceTest.java
+++ src/test/java/com/back/b2st/domain/venue/section/service/SectionServiceTest.java
@@ -3,6 +3,8 @@ package com.back.b2st.domain.venue.section.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.back.b2st.domain.venue.section.dto.response.SectionInfoRes;
 import com.back.b2st.domain.venue.section.entity.Section;
 import com.back.b2st.domain.venue.section.error.SectionErrorCode;
 import com.back.b2st.domain.venue.section.repository.SectionRepository;
+import com.back.b2st.domain.venue.venue.entity.Venue;
+import com.back.b2st.domain.venue.venue.repository.VenueRepository;
 import com.back.b2st.global.error.exception.BusinessException;
 
 @SpringBootTest
@@ -25,25 +30,32 @@ class SectionServiceTest {
 	private SectionService sectionService;
 	@Autowired
 	private SectionRepository sectionRepository;
+	@Autowired
+	private VenueRepository venueRepository;
 
+	private Venue venue1;
+	private Venue venue2;
 	private Section section1A;
 	private Section section1B;
 	private Section section2A;
 
 	@BeforeEach
 	void setUp() {
+		venue1 = venueRepository.save(new Venue("공연장1"));
+		venue2 = venueRepository.save(new Venue("공연장2"));
+
 		section1A = Section.builder()
-			.venueId(1L)
+			.venueId(venue1.getVenueId())
 			.sectionName("A")
 			.build();
 
 		section1B = Section.builder()
-			.venueId(1L)
+			.venueId(venue1.getVenueId())
 			.sectionName("B")
 			.build();
 
 		section2A = Section.builder()
-			.venueId(2L)
+			.venueId(venue2.getVenueId())
 			.sectionName("A")
 			.build();
 
@@ -79,5 +91,34 @@ class SectionServiceTest {
 
 		// then
 		assertThat(e.getErrorCode()).isEqualTo(SectionErrorCode.SECTION_NOT_FOUND);
+	}
+
+	@Test
+	@DisplayName("구역조회-성공-공연장")
+	void getSectionNameByVenue() throws Exception {
+		// given
+		Section section = section1A;
+		List<String> list = List.of("A", "B");
+
+		// when
+		SectionInfoRes findSection = sectionService.getSectionByVenueId(section.getVenueId());
+
+		// then
+		assertThat(findSection.venueId()).isEqualTo(section.getVenueId());
+		assertThat(findSection.sections().containsAll(list));
+	}
+
+	@Test
+	@DisplayName("구역조회_실패_공연장")
+	void getSectionNameBySection_fail_venue() throws Exception {
+		// given
+		Long venuId = 999L;
+
+		// when
+		BusinessException e = assertThrows(BusinessException.class,
+			() -> sectionService.getSectionByVenueId(venuId));
+
+		// then
+		assertThat(e.getErrorCode()).isEqualTo(SectionErrorCode.INVALID_VENUE_INFO);
 	}
 }

6efe8dddb8ba4705a504a63a491027709688d23c
weilim0513-tech <weilim0513@gmail.com>
2025-12-15 17:24:35 +0900

    feat: 환불 계좌 ê´등관리 api 구현

---
+++ src/main/java/com/back/b2st/global/common/dto/BankRes.java
@@ -0,0 +1,18 @@
+package com.back.b2st.global.common.dto;
+
+import com.back.b2st.global.common.BankCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BankRes {
+
+	private String code; // 서버
+	private String name; // 화면 표시
+
+	public static BankRes from(BankCode bankCode) {
+		return new BankRes(bankCode.getCode(), bankCode.getDescription());
+	}
+}

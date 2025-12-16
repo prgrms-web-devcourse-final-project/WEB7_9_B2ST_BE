cfe30bc4d6b31a94b7ce9c2c072c912112405b33
Nomi <codingworld@naver.com>
2025-12-16 10:07:52 +0900

    refactor: 좌석 id 추가

--- src/main/java/com/back/b2st/domain/seat/seat/dto/response/SeatInfoRes.java
+++ src/main/java/com/back/b2st/domain/seat/seat/dto/response/SeatInfoRes.java
@@ -3,12 +3,14 @@ package com.back.b2st.domain.seat.seat.dto.response;
 import com.back.b2st.domain.seat.seat.entity.Seat;
 
 public record SeatInfoRes(
+	Long seatId,
 	String sectionName,
 	String rowLabel,
 	Integer seatNumber
 ) {
 	public static SeatInfoRes toDetail(Seat seat) {
 		return new SeatInfoRes(
+			seat.getId();
 			seat.getSectionName(),
 			seat.getRowLabel(),
 			seat.getSeatNumber()

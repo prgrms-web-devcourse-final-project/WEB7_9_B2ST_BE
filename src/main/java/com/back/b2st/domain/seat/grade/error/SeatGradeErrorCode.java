a2ebf864e463a229e18264e5b1e240a25e0292ef
Nomi <codingworld@naver.com>
2025-12-17 11:51:43 +0900

    feat: 좌석 등급 생성 구현

---
+++ src/main/java/com/back/b2st/domain/seat/grade/error/SeatGradeErrorCode.java
@@ -0,0 +1,22 @@
+package com.back.b2st.domain.seat.grade.error;
+
+import org.springframework.http.HttpStatus;
+
+import com.back.b2st.global.error.code.ErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SeatGradeErrorCode implements ErrorCode {
+	PERFORMANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "S001", "공연 정보를 찾을 수 없습니다."),
+	SEAT_GRADE_NOT_FOUND(HttpStatus.NOT_FOUND, "S002", "좌석 정보를 찾을 수 없습니다."),
+	GRADE_REQUIRED(HttpStatus.BAD_REQUEST, "S003", "좌석 등급은 필수입니다."),
+	INVALID_GRADE_TYPE(HttpStatus.BAD_REQUEST, "S004", "유효하지 않은 좌석 등급입니다."),
+	ALREADY_CREATE_SEATGRADE(HttpStatus.CONFLICT, "S005", "이미 등록된 좌석등급입니다.");
+
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
+}
---
+++ src/test/java/com/back/b2st/domain/seat/grade/service/SeatGradeServiceTest.java
@@ -0,0 +1,122 @@
+package com.back.b2st.domain.seat.grade.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.back.b2st.domain.performance.entity.Performance;
+import com.back.b2st.domain.performance.entity.PerformanceStatus;
+import com.back.b2st.domain.performance.repository.PerformanceRepository;
+import com.back.b2st.domain.performanceschedule.entity.BookingType;
+import com.back.b2st.domain.performanceschedule.entity.PerformanceSchedule;
+import com.back.b2st.domain.performanceschedule.repository.PerformanceScheduleRepository;
+import com.back.b2st.domain.seat.grade.dto.request.CreateSeatGradeReq;
+import com.back.b2st.domain.seat.grade.dto.response.SeatGradeInfoRes;
+import com.back.b2st.domain.seat.grade.entity.SeatGrade;
+import com.back.b2st.domain.seat.grade.repository.SeatGradeRepository;
+import com.back.b2st.domain.seat.seat.entity.Seat;
+import com.back.b2st.domain.seat.seat.repository.SeatRepository;
+import com.back.b2st.domain.venue.section.entity.Section;
+import com.back.b2st.domain.venue.section.repository.SectionRepository;
+import com.back.b2st.domain.venue.venue.entity.Venue;
+import com.back.b2st.domain.venue.venue.repository.VenueRepository;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class SeatGradeServiceTest {
+
+	@Autowired
+	private SeatGradeService seatGradeService;
+	@Autowired
+	private SeatGradeRepository seatGradeRepository;
+	@Autowired
+	private PerformanceRepository performanceRepository;
+	@Autowired
+	private VenueRepository venueRepository;
+	@Autowired
+	private SectionRepository sectionRepository;
+	@Autowired
+	private SeatRepository seatRepository;
+	@Autowired
+	private PerformanceScheduleRepository performanceScheduleRepository;
+
+	private Performance performance;
+	private Section section;
+	private Seat seat;
+	private PerformanceSchedule performanceSchedule;
+
+	@BeforeEach
+	void setUp() {
+		// 공연, 좌석
+		Venue venue = venueRepository.save(Venue.builder()
+			.name("잠실실내체육관")
+			.build());
+
+		performance = performanceRepository.save(Performance.builder()
+			.venue(venue)
+			.title("2024 아이유 콘서트 - HEREH WORLD TOUR")
+			.category("콘서트")
+			.posterUrl("")
+			.description(null)
+			.startDate(LocalDateTime.of(2024, 12, 20, 19, 0))
+			.endDate(LocalDateTime.of(2024, 12, 22, 21, 0))
+			.status(PerformanceStatus.ON_SALE)
+			.build());
+
+		// Section 생성 추가
+		section = sectionRepository.save(Section.builder()
+			.venueId(venue.getVenueId())
+			.sectionName("A")
+			.build());
+
+		// Seat 생성 추가
+		seat = seatRepository.save(Seat.builder()
+			.venueId(venue.getVenueId())
+			.sectionId(section.getId())
+			.sectionName("A")
+			.rowLabel("8")
+			.seatNumber(7)
+			.build());
+
+		performanceSchedule = performanceScheduleRepository.save(
+			PerformanceSchedule.builder()
+				.performance(performance)
+				.startAt(LocalDateTime.of(2024, 12, 20, 19, 0))
+				.roundNo(1)
+				.bookingType(BookingType.LOTTERY)
+				.bookingOpenAt(LocalDateTime.of(2024, 12, 10, 12, 0))
+				.bookingCloseAt(LocalDateTime.of(2024, 12, 15, 23, 59))
+				.build());
+	}
+
+	@Test
+	@DisplayName("좌석등급 생성 - 성공")
+	void createSeatGradeInfo_success() {
+		// given
+		CreateSeatGradeReq req = new CreateSeatGradeReq(
+			performance.getPerformanceId(),
+			seat.getId(),
+			"VIP",
+			10000
+		);
+
+		// when
+		SeatGradeInfoRes saveGrade = seatGradeService.createSeatGradeInfo(performance.getPerformanceId(), req);
+
+		// then
+		assertThat(saveGrade.seatGradeId()).isNotNull();
+
+		SeatGrade findGrade = seatGradeRepository.findById(saveGrade.seatGradeId()).orElseThrow();
+		assertThat(findGrade.getId()).isEqualTo(saveGrade.seatGradeId());
+	}
+}

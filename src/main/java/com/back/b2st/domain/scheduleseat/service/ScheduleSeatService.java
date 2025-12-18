390f4a8185b3b3a5a19ebe29cae21b049fe7fcc3
yeongin <duddlsdlek610@gmail.com>
2025-12-18 15:01:38 +0900

    feat: 주석 추가

--- src/main/java/com/back/b2st/domain/scheduleseat/service/ScheduleSeatService.java
+++ src/main/java/com/back/b2st/domain/scheduleseat/service/ScheduleSeatService.java
@@ -17,13 +17,13 @@ public class ScheduleSeatService {
 
 	private final ScheduleSeatRepository scheduleSeatRepository;
 
-	/** 특정 회차 전체 좌석 조회 */
+	/** === 특정 회차 전체 좌석 조회 === */
 	@Transactional(readOnly = true)
 	public List<ScheduleSeatViewRes> getSeats(Long scheduleId) {
 		return scheduleSeatRepository.findSeats(scheduleId);
 	}
 
-	/** 특정 회차에서 상태별 좌석 조회 */
+	/** === 특정 회차에서 상태별 좌석 조회 === */
 	@Transactional(readOnly = true)
 	public List<ScheduleSeatViewRes> getSeatsByStatus(Long scheduleId, SeatStatus status) {
 		return scheduleSeatRepository.findSeatsByStatus(scheduleId, status);

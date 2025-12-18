c4f7bd93671c672945323a96c9b58a03b2fb16c7
Nomi <codingworld@naver.com>
2025-12-18 09:39:10 +0900

    refactor: 구역명 20자리 이하 조건 추가

--- src/main/java/com/back/b2st/domain/venue/section/dto/request/CreateSectionReq.java
+++ src/main/java/com/back/b2st/domain/venue/section/dto/request/CreateSectionReq.java
@@ -1,9 +1,11 @@
 package com.back.b2st.domain.venue.section.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CreateSectionReq(
 	@NotBlank(message = "구역명은 필수입니다.")
+	@Size(max = 20, message = "구역명은 20자 이하여야 합니다.")
 	String sectionName
 ) {
 }

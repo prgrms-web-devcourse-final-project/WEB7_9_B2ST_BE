bfbe9960f4d281c8158793eb8416486f1d63e841
Nomi <codingworld@naver.com>
2025-12-18 09:37:59 +0900

    refactor: Req 검증 수정(@Min -> @Positive)

--- src/main/java/com/back/b2st/domain/lottery/entry/dto/request/RegisterLotteryEntryReq.java
+++ src/main/java/com/back/b2st/domain/lottery/entry/dto/request/RegisterLotteryEntryReq.java
@@ -1,8 +1,8 @@
 package com.back.b2st.domain.lottery.entry.dto.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record RegisterLotteryEntryReq(
 	@NotNull(message = "사용자ID는 필수입니다.")
@@ -15,7 +15,7 @@ public record RegisterLotteryEntryReq(
 	String grade,
 
 	@NotNull(message = "인원수는 필수입니다.")
-	@Min(value = 1, message = "인원수는 1 이상이어야 합니다.")
+	@Positive(message = "인원수는 1 이상이어야 합니다.")
 	Integer quantity
 ) {
 }
--- src/main/java/com/back/b2st/domain/seat/grade/dto/request/CreateSeatGradeReq.java
+++ src/main/java/com/back/b2st/domain/seat/grade/dto/request/CreateSeatGradeReq.java
@@ -1,14 +1,14 @@
 package com.back.b2st.domain.seat.grade.dto.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record CreateSeatGradeReq(
 	@NotNull(message = "좌석id는 필수 입니다.")
 	Long seatId,
 	String grade,
 	@NotNull(message = "가격은 필수 입니다.")
-	@Min(value = 1, message = "가격은 1 이상이어야 합니다.")
+	@Positive(message = "가격은 1 이상이어야 합니다.")
 	Integer price
 ) {
 }

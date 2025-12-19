3768421defdb8428961396a22ae828aa6ea2890e
yeongin <duddlsdlek610@gmail.com>
2025-12-20 01:12:37 +0900

    refactor: 상태 추가

--- src/main/java/com/back/b2st/domain/reservation/entity/ReservationStatus.java
+++ src/main/java/com/back/b2st/domain/reservation/entity/ReservationStatus.java
@@ -1,8 +1,9 @@
 package com.back.b2st.domain.reservation.entity;
 
 public enum ReservationStatus {
-	PENDING,    // 결제 대기 (무통장)
-	PAID,    // 결제 완료
+	CREATED, // 예매 생성
+	PENDING,    // 무통장 입금 대기
+	PAID,    // 결제 승인 완료 (카드 승인, 무통장 입금 확인)
 	COMPLETED, // 확정
 	CANCELED,    // 취소
 	EXPIRED    // 결제 기한 초과

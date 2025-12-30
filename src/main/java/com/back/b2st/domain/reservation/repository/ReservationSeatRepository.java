02c8e2e685dd9d6c3d53ecadf5fa99ad50900b3f
yeongin <duddlsdlek610@gmail.com>
2025-12-30 15:41:28 +0900

    refactor: 레포 추가

---
+++ src/main/java/com/back/b2st/domain/reservation/repository/ReservationSeatRepository.java
@@ -0,0 +1,17 @@
+package com.back.b2st.domain.reservation.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.back.b2st.domain.reservation.dto.response.ReservationSeatInfo;
+import com.back.b2st.domain.reservation.entity.ReservationSeat;
+
+@Repository
+public interface ReservationSeatRepository extends JpaRepository<ReservationSeat, Long> {
+
+	List<ReservationSeatInfo> findSeatInfos(Long reservationId);
+
+	List<ReservationSeat> findByReservationId(Long reservationId);
+}

99442cb6107d1df118e4a6a6c90a089b3ba9098e
Chehyeon-Kim23 <rlacogus06090@gmail.com>
2026-01-04 04:05:00 +0900

    fix:워크플로우 수정 (#421)
    * fix:ãìci 테스트 통과ë
    
    * fix:워크플로우 수정
    
    * fix:워크플로우 복귀
    
    * fix:워크플로우 ë수정

--- src/test/java/com/back/b2st/global/config/TestMailConfig.java
+++ src/test/java/com/back/b2st/global/config/TestMailConfig.java
@@ -1,18 +1,16 @@
 package com.back.b2st.global.config;
 
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import jakarta.mail.internet.MimeMessage;
 
-/**
- * 테스트 환경에서 JavaMailSender를 Mock으로 대체
- * 실제 SMTP 서버 연결 없이 테스트 가능
- */
-@TestConfiguration
+@Configuration
+@Profile("test")
 public class TestMailConfig {
 
 	@Bean
@@ -20,7 +18,9 @@ public class TestMailConfig {
 	public JavaMailSender javaMailSender() {
 		JavaMailSender mailSender = Mockito.mock(JavaMailSender.class);
 		MimeMessage mimeMessage = Mockito.mock(MimeMessage.class);
+
 		Mockito.when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
+		// send()는 void라 기본이 doNothing()이라 SMTP 연결 자체를 안 함
 		return mailSender;
 	}
 }
